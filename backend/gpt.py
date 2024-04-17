50ee767427434a18b0223988bfcd02874061f1b9
danielp1218 <danielpu2007@gmail.com>
2024-04-17 19:51:27 -0400

    initial api commit

--- backend/gpt.py
+++ backend/gpt.py
@@ -1,56 +1,56 @@
 import base64
 import requests
-
 from keys import OPENAI_API_KEY
+
+from blacksheep import FromFiles, post
+
+
 
 # OpenAI API Key
 api_key = OPENAI_API_KEY
+@post("/")
+async def post_data(files: FromFiles):
+    data = files.value
+    image = data[0].data
+
+    # Getting the base64 string
+    base64_image = base64.b64encode(image)
+
+    headers = {
+      "Content-Type": "application/json",
+      "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+      "model": "gpt-4-turbo",
+      "messages": [
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text": "What's in this image?"
+            },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}",
+                "detail": "low"
+              }
+            }
+          ]
+        }
+      ],
+      "max_tokens": 200
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    return response.json()
+
+
+
 
 # Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
-
-# Path to your image
-name = 'roadsign2'
-image_path = f"{name}.png"
-
-# Getting the base64 string
-base64_image = encode_image(image_path)
-
-headers = {
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {api_key}"
-}
-
-payload = {
-  "model": "gpt-4-turbo",
-  "messages": [
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "What's in this image?"
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}",
-            "detail": "low"
-          }
-        }
-      ]
-    }
-  ],
-  "max_tokens": 200
-}
-
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-
-import json
-
-with open(f'{name}low.json', 'w') as f:
-    a = response.json()
-    print(a)
-    json.dump(a, f, indent=4)
