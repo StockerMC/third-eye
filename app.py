a5d184291ea5ba07fa052ab16fa934cf0533aebf
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-04-17 21:03:30 -0400

    Initial backend

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+import waitress
+import base64
+import requests
+from keys import OPENAI_API_KEY
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def test():
+    return '<p>hi</p>'
+
+@app.route("/image", methods=['GET'])
+def hello_world():
+
+    # Getting the base64 string
+    # base64_image = encode_image(image_path)
+    base64_image = request.args.get('data')
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_API_KEY}"
+    }
+
+    payload = {
+        "model": "gpt-4-turbo",
+        "messages": [
+            {
+            "role": "user",
+            "content": [
+                {
+                "type": "text",
+                "text": "What's in this image?"
+                },
+                {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{base64_image}",
+                    "detail": "low"
+                }
+                }
+            ]
+            }
+        ],
+        "max_tokens": 200
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)    
+    return response.json()
+
+waitress.serve(app, listen='0.0.0.0:5003')
